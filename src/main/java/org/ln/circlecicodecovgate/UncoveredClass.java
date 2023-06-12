d79049f042628bd6c8279f8cddd6291f36907e0d
ln0 <lewis@lewisnotes.com>
2023-06-12 00:53:27 -0700

    add additional java code to break coverage

--- src/main/java/org/ln/circlecicodecovgate/UncoveredClass.java
+++ src/main/java/org/ln/circlecicodecovgate/UncoveredClass.java
@@ -12,4 +12,16 @@ public class UncoveredClass {
 
         return Integer.MAX_VALUE;
     }
+
+    public int isNineOrTenOrMaxValue_additional(String input) {
+        if (input.equals("nine")) {
+            return 9;
+        }
+
+        if (input.equals("ten")) {
+            return 10;
+        }
+
+        return Integer.MAX_VALUE;
+    }
 }
